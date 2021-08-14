215cace522f449e968099a4756eb4e5799bf21ef
kwanele <kwanelegamedze4@gmail.com>
2021-08-14 15:07:09 +0200

    fixed the non-working percent buttons

--- script.js
+++ script.js
@@ -5,30 +5,29 @@ const submit = document.getElementById("resert_btn")
 const custom = document.getElementById("percent_custom")
 const tip = document.getElementById("tip_amount")
 const total = document.getElementById("total_amount")
-let current = document.querySelector(".active")
+let current = document.getElementsByClassName("active")
 
 smallBtns.forEach((el, key) => {
     el.addEventListener('click', () => {
         current[0].className = current[0].className.replace("active", " ")
-        el.classList.add("active")
-        custom.classList.remove("touch")
+        el.className += " active"
     })
 })
 
 custom.addEventListener("click", ()=> {
-    custom.classList.add("touch")  
+    custom.classList.add("touch")
 })
 
 submit.addEventListener('click', () => {
     if (bill.value > 0) {
         document.querySelector(".warning").style.visibility = "hidden"
-        
+
         if (people.value > 0) {
             document.querySelector(".warning2").style.visibility = "hidden"
             const active = document.getElementsByClassName("active")
             let answer = null
-            
-            if (custom.value > 0) {
+
+            if (custom.value > 1) {
                 answer =  (parseFloat(bill.value) * parseFloat(custom.value))
             } else {
                 answer = (parseFloat(bill.value) * parseFloat(active[0].innerText))
@@ -39,7 +38,7 @@ submit.addEventListener('click', () => {
             tip.innerText = ti + "$"
 
             const to = ((parseFloat(bill.value) / parseFloat(people.value)) + parseFloat(ti)).toFixed(2)
-            
+
             total.innerText = to + "$"
         }
         else {
@@ -49,4 +48,4 @@ submit.addEventListener('click', () => {
     else {
         document.querySelector(".warning").style.visibility = "visible"
     }
-})
+}) 
